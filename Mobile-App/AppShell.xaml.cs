df03cf5548599057e24beb6a1d2093dd3aa4d3b1
Boy Stroo <1061579@hr.nl>
2023-10-20 09:44:05 +0200

    Add project files.

---
+++ Mobile-App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Mobile_App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Mobile-App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Mobile_App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
