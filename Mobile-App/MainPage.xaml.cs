324c173e1fdc00518eeb840b4acff457e7b23e38
Boy Stroo <1061579@hr.nl>
2023-10-24 13:09:19 +0200

    Figured out navigation/routing and created a new page called MyPage that also routes back.

--- Mobile-App/MainPage.xaml.cs
+++ Mobile-App/MainPage.xaml.cs
@@ -9,16 +9,9 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            await Navigation.PushAsync(new MyPage());
         }
     }
 }
