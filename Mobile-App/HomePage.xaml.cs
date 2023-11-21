b7507767e10c9ad3ca9f93ff4799676a118eff57
TikTax28 <tommy.manh.hung@gmail.com>
2023-11-21 10:23:07 +0100

    renamed mypage to homepage and fixed corresponding errors

---
+++ Mobile-App/HomePage.xaml.cs
@@ -0,0 +1,33 @@
+namespace Mobile_App;
+
+public partial class HomePage : ContentPage
+{
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+
+    private void PreviousWeekButton_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync("..");
+    }
+
+    private void NextWeekButton_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync("..");
+    }
+    private async void Button_OnClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("..");
+    }
+
+    private async void OnSettingsClicked(object sender, EventArgs e)
+    {
+        var button = (ImageButton)sender;
+        button.IsEnabled = false;
+        await Navigation.PushAsync(new SettingsPage());
+        button.IsEnabled = true;
+    }
+
+
+}
