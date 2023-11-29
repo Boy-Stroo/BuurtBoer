58fdb298628edcc0b75fd907dc2a8aa4ca37edf3
Boy Stroo <1061579@hr.nl>
2023-11-29 16:42:06 +0100

    Set default launch site to swagger/index.html

--- Server-Things/Program.cs
+++ Server-Things/Program.cs
@@ -25,7 +25,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => 
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseSession();
