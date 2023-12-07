5b6b840fd61b03b29d8e45914303efac12773775
Boy Stroo <1061579@hr.nl>
2023-12-07 14:00:17 +0100

    GetAllCompanies

---
+++ Web-App/Controllers/CompanyController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web_App
+{
+    public class CompanyController
+    {
+        private readonly CompanyService _companyService;
+        private ObservableCollection<Company> _companies;
+        public ObservableCollection<Company> Companies
+        {
+            get => _companies;
+            set => _companies = value;
+        }
+        private Company? _currentCompany;
+        public Company? CurrentCompany
+        {
+            get => _currentCompany;
+            private set => _currentCompany = value;
+        }
+        public CompanyController()
+        {
+            _companyService = new();
+            _companies = new ObservableCollection<Company>();
+        }
+
+        public CompanyController(CompanyService userService)
+        {
+            _companyService = userService;
+            _companies = new ObservableCollection<Company>();
+        }
+        public async Task GetAllCompanies()
+        {
+            Companies = await _companyService.GetAll();
+        }
+    }
+}
---
+++ Web-App/Services/CompanyService.cs
@@ -0,0 +1,29 @@
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text.Json;
+using Web_App;
+
+public class CompanyService : HTTPService
+{
+    public async Task<ObservableCollection<Company>> GetAll()
+    {
+
+        try
+        {
+            // Call to the backend
+            HttpResponseMessage response = await _client.GetAsync($"{_domain}/api/user/all");
+            // If the call is successful, read the response and deserialize it into a list of users
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<ObservableCollection<Company>>(content);
+            }
+        }
+        // Too broad of an exception, but yeah
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"ERROR {ex.Message}");
+        }
+        return null;
+    }
+}
