fa97e0e22468e16b8662558fe4593e49782e035a
TikTax28 <tommy.manh.hung@gmail.com>
2024-01-13 19:03:04 +0100

    testdb tommy

---
+++ Server-Things/Migrations/20240113180238_test123db.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server_Things.Migrations
+{
+    /// <inheritdoc />
+    public partial class test123db : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
