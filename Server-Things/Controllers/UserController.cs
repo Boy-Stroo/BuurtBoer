0a3be1e7644c9db0b1ee771fd7da8acb8434c076
Boy-Stroo <boytjuh1995@gmail.com>
2024-01-15 17:32:16 +0100

    Removed interface

--- Server-Things/Controllers/UserController.cs
+++ Server-Things/Controllers/UserController.cs
@@ -3,7 +3,6 @@ using System.Text.Json;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Server_Things.Interfaces;
 using Server_Things.Models;
 using Server_Things.Factories;
 
